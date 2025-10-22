c901f96eb484fa6dc0051cad227e2ebdbc942173
SuperSimpleDev <aarushi.gupta102006@gmail.com>
2025-10-22 13:40:00 +0530

    create automatied test for format currency

---
+++ tests/moneyTest.js
@@ -0,0 +1,12 @@
+import { formatCurrency } from "../scripts/utils/money.js";
+
+if (formatCurrency(2095) === '20.95') {
+  console.log('passed');
+} else {
+  console.log('fail');
+}
+if (formatCurrency(2000.5) === '20.01') {
+  console.log('passed');
+} else {
+  console.log('fail');
+}
